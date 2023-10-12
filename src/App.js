fb881a38f9d4bf279fb1c366280e28bb8dfd070f
hamzasheraz <hamzasheraz429@gmail.com>
2023-10-12 22:17:45 +0500

    Adding files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Temp from "./component/weather/temp";
+
+function App() {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/weather/temp.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import Weathercard from "./weathercard";
+import "./style.css";
+
+const Temp = () => {
+  const [searchValue, setSearchValue] = useState("Lahore");
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=3d448ab05666cb049f3b4e9a506c46da`;
+
+      let res = await fetch(url);
+      let data = await res.json();
+
+      const { temp, humidity, pressure } = data.main; //destructuring
+      const { main: weathermood } = data.weather[0]; //destructurin and also changing the name
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        //object
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      {/* our temp card  */}
+      <Weathercard {...tempInfo} />
+      {/* by default destructuring  */}
+    </>
+  );
+};
+
+export default Temp;
